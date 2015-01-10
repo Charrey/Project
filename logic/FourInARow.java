0847dcdd24b714f8695002681812c74c859a6ede
Boschma <reneboschma@hotmail.com>
2015-01-10 15:16:18 +0100

    AI update
    AI wordt weergegeven, nog geen interactie

--- logic/FourInARow.java
+++ logic/FourInARow.java
@@ -1,9 +1,13 @@
 package Project.logic;
 
+import Project.gui.Main;
+
 public class FourInARow {
 
 	public static void main(String[] args) {
+		new Main(new Board());
 		new Game(new HumanPlayer(args[0], Mark.O), new ComputerPlayer(args[1], Mark.X)).start();
+		
 	}
-//test
+
 }
--- logic/Mark.java
+++ logic/Mark.java
@@ -23,5 +23,7 @@ public enum Mark {
 			return " ";
 		}
 	}
+	
+
 
 }
