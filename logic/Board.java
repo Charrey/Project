951cf90216905ac1346e09ee15255f2e57fcf664
Boschma <reneboschma@hotmail.com>
2015-01-15 19:01:10 +0100

    JML BITCHES
    Board heeft zojuist de onnodige JML code ontvangen. Methodes geschreven
    door Pim moeten nog worden voorzien.

--- logic/Board.java
+++ logic/Board.java
@@ -26,6 +26,13 @@ public class Board extends Observable {
 	 * @param heigth
 	 *            is the heigth of the board.
 	 */
+	/*@
+	 * requires width>0 && height>0;
+	 * ensures win == false;
+	 * loop_invariant i>=0 && i<width;
+	 * loop_invariant p>=0 && p<height;
+	 * ensures (\forall x; x>=0 && x<width; \forall y; y>=0 && y<height; board[x][p].equals(Mark.EMPTY)); 
+	 */
 	public Board(int width, int height) {
 		this.width = width;
 		this.height = height;
@@ -47,6 +54,13 @@ public class Board extends Observable {
 	 *            is the mark that needs to be put into the board
 	 * @return if the mark is placed into the board, returns the row, else
 	 *         return -1.
+	 */
+	/*@
+	 * requires column >=0 && column<width && ! m==null;
+	 * loop_invariant i>=0 && i<height;
+	 * ensures columnFree(column) == false ==> \result == -1;
+	 * ensures columnFree(column) == true ==> \result == (\min j; j>=0 && j<getHeight() && getPlace(column, j).equals(Mark.Empty));
+	 * ensures columnFree(column == true ==> win == checkWin(column, /result); 
 	 */
 	public int putMark(int column, Mark m) {
 		for (int i = 0; i < this.height; i++) {
@@ -70,6 +84,12 @@ public class Board extends Observable {
 	 *            is the row the mark needs to be placed into.
 	 * @param m
 	 *            is the mark that needs to be placed.
+	 */
+	/*@
+	 * requires width>=0 && width<getWidth();
+	 * requires height>=0 && height<getHeight();
+	 * requires ! m==null;
+	 * ensures board[width][height] = m;
 	 */
 	public void putMark(int width, int height, Mark m) {
 		board[width][height] = m;
@@ -113,6 +133,10 @@ public class Board extends Observable {
 	 * 
 	 * @return returns true if the board is full, else returns false.
 	 */
+	/*@
+	 * loop_invariant i>=0 && i<getWidth();
+	 * ensures \result == (\exist j; j>=0 && j<getWidth() && board[j][getHeight-1].equals(Mark.EMPTY);
+	 */
 	public boolean isFull() {
 		for (int i = 0; i < getWidth(); i++) {
 			if (board[i][this.height - 1].equals(Mark.EMPTY)) {
@@ -127,6 +151,10 @@ public class Board extends Observable {
 	 * 
 	 * @return true if there is a winner, else false.
 	 */
+	/*@
+	 * pure
+	 * ensures \result == win;
+	 */
 	public boolean isWin() {
 		return win;
 	}
@@ -135,6 +163,10 @@ public class Board extends Observable {
 	 * Checks if the board is full or there is a winner.
 	 * 
 	 * @return returns (isWin() or isFull()).
+	 */
+	/*@
+	 * pure
+	 * ensures \result == isWin() || isFull();
 	 */
 	public boolean gameOver() {
 		return isWin() || isFull();
@@ -147,6 +179,10 @@ public class Board extends Observable {
 	 *            is the column to be checked
 	 * @return true if column is free, else false.
 	 */
+	/*@
+	 * requires input>=0 && input<getWidth();
+	 * ensures \result == board[input][getHeight()-1].equals(Mark.EMPTY);
+	 */
 	public boolean columnFree(int input) {
 		return (board[input][height - 1].equals(Mark.EMPTY));
 	}
@@ -155,6 +191,11 @@ public class Board extends Observable {
 	 * Makes a copy of the current board.
 	 * 
 	 * @return returns a copy of the current board.
+	 */
+	/*@
+	 * loop_invariant i>=0 && i<getWidht();
+	 * loop_invartiant p>=0 && p<getHeight();
+	 * ensures \forall((\forall x; x>=0 && x<getWidth(); \forall y; y>=0 && y<getHeight(); \result.getPlace(x,y).equals(getPlace(x,y)) 
 	 */
 	public Board copy() {
 		Board board = new Board(width, height);
@@ -174,6 +215,9 @@ public class Board extends Observable {
 	 *            need to be checked
 	 * @return true if column is valid, else false.
 	 */
+	/*@
+	 * ensures /result == column>=0 && column<getWidth();
+	 */
 	public boolean isValidInput(int column) {
 		return (column >= 0 && column < width);
 	}
@@ -184,6 +228,11 @@ public class Board extends Observable {
 	 * @param column
 	 *            that needs to be checked
 	 * @return return the amount of empty marks in the board.
+	 */
+	/*@
+	 * requires isValidInput(column) == true;
+	 * loop_invariant i>=0 && i<getHeight();
+	 * ensures \result == \sum(\forall y; y>=0 && y<getHeight() && getPlace(column,y).equals(Mark.EMPTY));
 	 */
 	public int emptySlotCount(int column) {
 		int count = 0;
@@ -200,6 +249,10 @@ public class Board extends Observable {
 	 * 
 	 * @return width of the board.
 	 */
+	/*@
+	 * pure
+	 * ensures \result == width;
+	 */
 	public int getWidth() {
 		return width;
 	}
@@ -209,6 +262,10 @@ public class Board extends Observable {
 	 * 
 	 * @return height of the board.
 	 */
+	/*@
+	 * pure
+	 * ensures \result == height;
+	 */
 	public int getHeight() {
 		return height;
 	}
@@ -217,6 +274,10 @@ public class Board extends Observable {
 	 * Gets the Mark 2d array representation of the board
 	 * 
 	 * @return returns the mark represenation of the board
+	 */
+	/*@
+	 * ensures \forall(\forall int x; x>=0 && x<getWidht(); \forall int y; y>=0 && y<=getHeight(); \result[x][y].equals(getPlace(x,y));
+	 * ensures \result == board;
 	 */
 	public Mark[][] getBoard() {
 		return board;
@@ -231,12 +292,24 @@ public class Board extends Observable {
 	 *            of the place that needs to be checked
 	 * @return the mark allocated in the given column and row
 	 */
+	/*@
+	 * pure
+	 * requires column>=0 && column<getWidth();
+	 * requires row>=0; && row<getHeight();
+	 * ensures \result == board[column][row];
+	 */
 	public Mark getPlace(int column, int row) {
 		return board[column][row];
 	}
 
 	/**
 	 * Resets the board.
+	 */
+	/*@
+	 * ensures win = false;
+	 * loop_invariant i>=0 && i<getWidth();
+	 * loop_invariant p>=0 && p<getheight();
+	 * ensures \forall(\forall int x; x>=0 && x<getWidht(); \forall int y; y>=0 && y<=getHeight(); board[x][y].equals(Mark.EMPTY);
 	 */
 	public void reset() {
 		win = false;
@@ -273,6 +346,10 @@ public class Board extends Observable {
 	 * @param m
 	 *            is the mark of which the AI needs to determine the move
 	 * @return the move the AI would have made.
+	 */
+	/*@
+	 * requires ! m==null;
+	 * 
 	 */
 	public int hint(Mark m) {
 		return new ComputerPlayer(m).determineMove(this);
