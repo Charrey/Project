9be77ee1f0b041f61538235857f92101ca403c85
Boschma <reneboschma@hotmail.com>
2015-01-20 13:28:15 +0100

    update

--- gui/MessagePanel.java
+++ gui/MessagePanel.java
@@ -2,10 +2,15 @@ package Project.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.TextArea;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -17,6 +22,11 @@ public class MessagePanel extends JPanel{
 	
 	private Game g;
 	private JPanel invoerButtonPanel;
+	private JTextArea textarea;
+	private JButton button;
+	private JTextField textfield;
+	private JScrollPane scrollPane;
+	
 	
 	public MessagePanel(Game g){
 		this.g = g;
@@ -25,17 +35,31 @@ public class MessagePanel extends JPanel{
 		
 		
 		invoerButtonPanel = new JPanel();
-		invoerButtonPanel.setLayout(new GridLayout(1,2));
+		invoerButtonPanel.setLayout(new FlowLayout());
 		
-		JTextField textfield = new JTextField("Je kan hier typen", WIDTH);
-		JButton button = new JButton("Verzend");
-		//TODO button smaller
-		//TODO add textarea
-		button.setMaximumSize(new Dimension(textfield.getHeight(), WIDTH/10));
+		textfield = new JTextField("Je kan hier typen", WIDTH);
+		button = new JButton("Verzend");
+		textarea = new JTextArea();
+		scrollPane = new JScrollPane(textarea);
+
+		button.setMaximumSize(new Dimension(textfield.getHeight(), WIDTH/2));
+		textarea.setEditable(false);
 		
 		invoerButtonPanel.add(textfield);
 		invoerButtonPanel.add(button);
 		add(invoerButtonPanel, BorderLayout.PAGE_END);
+		add(scrollPane, BorderLayout.CENTER);
+
+		button.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				addMessage(textfield.getText());
+				textfield.setText("");
+			}
+		});
+	}
+	
+	public void addMessage(String msg){
+		textarea.append(msg + "\n");
 	}
 	
 }
