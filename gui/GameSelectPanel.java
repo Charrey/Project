b2e7f073b5cdba5381f59e3724d65acf17184770
Boschma <reneboschma@hotmail.com>
2015-01-22 18:11:24 +0100

    gameselectpanel overhaul
    overhaul

--- gui/GameSelectPanel.java
+++ gui/GameSelectPanel.java
@@ -1,19 +1,15 @@
 package Project.gui;
 
-import java.awt.Dimension;
+import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
-
-import Project.logic.Board;
 import Project.logic.ComputerPlayer;
 import Project.logic.Game;
 import Project.logic.HumanPlayer;
@@ -22,134 +18,168 @@ import Project.logic.Mark;
 import Project.logic.Player;
 
 public class GameSelectPanel extends JPanel {
-
-	private int choice1;
-	private int choice2;
-	private Player player1;
-	private Player player2;
-	private MainGui gui;
+	
+	private Font fnt0 = new Font("", Font.BOLD, 15);
 	private Font fnt = new Font("Serif", Font.BOLD, 30);
+	private Font fnt1 = new Font("Serif", Font.ITALIC, 15);
+	private Font titleFont = new Font("", Font.BOLD, 60);
 	private EmptyBorder padding = new EmptyBorder(10,10,10,10);
+	private EmptyBorder titlePadding = new EmptyBorder(40,40,40,40);
+	
+	private JLabel optionsLabel;
+		private JLabel player1Label;
+			private JLabel infoLabel1;
+			private JTextField player1Field;
+		private JLabel player2Label;
+			private JLabel infoLabel2;
+			private JTextField player2Field;
+		private JLabel boardSizeLabel;
+			private JLabel widthLabel;
+			private JTextField widthField;
+			private JLabel heightLabel;
+			private JTextField heightField;
+	private JButton startButton;
+	
+	private InputHandler handler;
 	
 	public GameSelectPanel(MainGui gui){
-		this.gui = gui;
+
 	
-		this.setLayout(new GridLayout(3,2));
-		Dimension d = new Dimension(200,200);
-		JLabel newHumanPlayerLabel1 = new JLabel("Human Player");
-		newHumanPlayerLabel1.setHorizontalAlignment(SwingConstants.CENTER);
-		newHumanPlayerLabel1.setMinimumSize(d);
+		setLayout(new BorderLayout());
 		
-		JLabel newHumanPlayerLabel2 = new JLabel("Human Player");
-		newHumanPlayerLabel2.setHorizontalAlignment(SwingConstants.CENTER);
-		newHumanPlayerLabel2.setMinimumSize(d);
+		JPanel combinePanel = new JPanel();
+		combinePanel.setLayout(new BorderLayout()); //om playerLabels en playerSelectPanel te mergen.
 		
-		JLabel newAIPlayerLabel1 = new JLabel("AI Player");
-		newAIPlayerLabel1.setHorizontalAlignment(SwingConstants.CENTER);
-		newAIPlayerLabel1.setMinimumSize(d);
+		JPanel combinePanel2 = new JPanel();
+		combinePanel2.setLayout(new BorderLayout()); //om combinePanel en boardPanel te mergen
 		
-		JLabel newAIPlayerLabel2 = new JLabel("AI Player");
-		newAIPlayerLabel2.setHorizontalAlignment(SwingConstants.CENTER);
-		newAIPlayerLabel2.setMinimumSize(d);
+		optionsLabel = new JLabel("Options");
+		optionsLabel.setFont(titleFont);
+		optionsLabel.setBorder(titlePadding);
+		add(optionsLabel, BorderLayout.PAGE_START);
 		
-		JLabel startGameLabel = new JLabel("Start Game");
-		startGameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		startGameLabel.setMinimumSize(d);
+		JPanel playerLabels = new JPanel();
+		playerLabels.setLayout(new GridLayout(1,2));
 		
-		newHumanPlayerLabel1.setFont(fnt);
-		newHumanPlayerLabel2.setFont(fnt);
-		newAIPlayerLabel1.setFont(fnt);
-		newAIPlayerLabel2.setFont(fnt);
-		startGameLabel.setFont(fnt);
+		player1Label = new JLabel("Select player 1");
+		player1Label.setFont(fnt);
+		player1Label.setBorder(padding);
+		playerLabels.add(player1Label);
 		
-		newHumanPlayerLabel1.setBorder(padding);
-		newHumanPlayerLabel2.setBorder(padding);
-		newAIPlayerLabel1.setBorder(padding);
-		newAIPlayerLabel2.setBorder(padding);
-		startGameLabel.setBorder(padding);
+		player2Label = new JLabel("Select player 2");
+		player2Label.setFont(fnt);
+		player2Label.setBorder(padding);
+		playerLabels.add(player2Label);
 		
-		newHumanPlayerLabel1.addMouseListener(new MouseAdapter(){
-			public void mouseClicked(MouseEvent e){
-				choice1 = 0;
-				System.out.println("1 = 0");
-			}
-		});
 		
-		newHumanPlayerLabel2.addMouseListener(new MouseAdapter(){
-			public void mouseClicked(MouseEvent e){
-				choice2 = 0;
-				System.out.println("2 = 0");
-			}
-		});
-		
-		newAIPlayerLabel1.addMouseListener(new MouseAdapter(){
-			public void mouseClicked(MouseEvent e){
-				choice1 = 1;
-				System.out.println("1 = 1");
+		JPanel playerSelectPanel = new JPanel();
+		playerSelectPanel.setLayout(new GridLayout(2,2));
+		infoLabel1 = new JLabel("(Keep empty for AI)");
+		infoLabel1.setFont(fnt1);
+		//infoLabel1.setBorder(padding);
+		playerSelectPanel.add(infoLabel1);
 
-			}
-		});
+		infoLabel2 = new JLabel("(Keep empty for AI)");
+		infoLabel2.setFont(fnt1);
+		//infoLabel2.setBorder(padding);
+		playerSelectPanel.add(infoLabel2);
 		
-		newAIPlayerLabel2.addMouseListener(new MouseAdapter(){
+		player1Field = new JTextField();
+		player1Field.setFont(fnt0);
+		//player1Field.setBorder(padding);
+		playerSelectPanel.add(player1Field);
+		
+		player2Field = new JTextField();
+		player2Field.setFont(fnt0);
+		//player2Field.setBorder(padding);
+		playerSelectPanel.add(player2Field);
+		
+		combinePanel.add(playerLabels, BorderLayout.PAGE_START);
+		combinePanel.add(playerSelectPanel, BorderLayout.CENTER);
+		
+		//add(combinePanel, BorderLayout.CENTER);
+		
+		
+		JPanel boardPanel = new JPanel();
+		boardPanel.setLayout(new BorderLayout());
+		
+		boardSizeLabel = new JLabel("Board Settings");
+		boardSizeLabel.setFont(fnt);
+		boardSizeLabel.setBorder(padding);
+		boardPanel.add(boardSizeLabel, BorderLayout.PAGE_START);
+		
+		JPanel boardGridPanel = new JPanel();
+		boardGridPanel.setLayout(new GridLayout(2,2));
+		
+		widthLabel = new JLabel("Widht of Field");
+		widthLabel.setFont(fnt0);
+		boardGridPanel.add(widthLabel);
+		
+		widthField = new JTextField("7");
+		widthField.setFont(fnt0);
+		boardGridPanel.add(widthField);
+		
+		heightLabel = new JLabel("Height of Field");
+		heightLabel.setFont(fnt0);
+		boardGridPanel.add(heightLabel);
+		
+		heightField = new JTextField("6");
+		heightField.setFont(fnt0);
+		boardGridPanel.add(heightField);
+		
+		boardPanel.add(boardGridPanel, BorderLayout.CENTER);
+		
+		combinePanel2.add(combinePanel, BorderLayout.PAGE_START);
+		combinePanel2.add(boardPanel, BorderLayout.CENTER);
+		
+		add(combinePanel2, BorderLayout.CENTER);
+		
+		startButton = new JButton("Start Game");
+		startButton.setFont(fnt0);
+		
+		add(startButton, BorderLayout.PAGE_END);
+		
+		
+		startButton.addMouseListener(new MouseAdapter(){
 			public void mouseClicked(MouseEvent e){
-				choice2 = 1;
-				System.out.println("2 = 1");
+				String player1 = player1Field.getText();
+				String player2 = player2Field.getText();
+				
+				int width = Integer.parseInt(widthField.getText());
+				int height = Integer.parseInt(heightField.getText());
+				Player p1;
+				Player p2;
+				
 
-			}
-		});
-		
-		startGameLabel.addMouseListener(new MouseAdapter(){
-			public void mouseClicked(MouseEvent e){
-				if(choice1 == 0 && choice2 ==0){
-					InputHandler handler = new InputHandler();
-					player1 = new HumanPlayer("Speler1", Mark.X, handler);
-					player2 = new HumanPlayer("Speler2", Mark.O, handler);
-					//Game newGame = new Game(player1, player2, gui);
-					Game newGame = new Game(player1, player2, gui);
-					gui.changePanel(new GameMainPanel(newGame, handler));
-					new Thread(newGame).start();
-				}else if(choice1==1&&choice2==1){
-					player1 = new ComputerPlayer(Mark.X);
-					player2 = new ComputerPlayer(Mark.O);
-					Game newGame = new Game(player1, player2, gui);
-					gui.changePanel(new GameMainPanel(newGame));
-					new Thread(newGame).start();
-				}else {
-					InputHandler handler = new InputHandler();
-					player1 = new HumanPlayer("Speler1", Mark.X, handler);
-					player2 = new ComputerPlayer(Mark.O);
-					Game newGame = new Game(player1, player2, gui);
-					gui.changePanel(new GameMainPanel(newGame, handler));
-					new Thread(newGame).start();
-					
+				if(!player1.isEmpty() && !player2.isEmpty()){
+					handler = new InputHandler();
+					p1 = new HumanPlayer(player1, Mark.X, handler);
+					p2 = new HumanPlayer(player2, Mark.O, handler);
+				}else if(!player1.isEmpty() && player2.isEmpty()){
+					handler = new InputHandler();
+					p1 = new HumanPlayer(player1, Mark.X, handler);
+					p2 = new ComputerPlayer(Mark.O);
+				}else if(player1.isEmpty() && !player2.isEmpty()){
+					handler = new InputHandler();
+					p1 = new ComputerPlayer(Mark.X);
+					p2 = new HumanPlayer(player1, Mark.O, handler);
+				}else{
+					p1 = new ComputerPlayer(Mark.X);
+					p2 = new ComputerPlayer(Mark.O);
 				}
 				
-				
-				//Game newGame = new Game(player1, player2, gui);
-				//Game newGame = new Game(new ComputerPlayer(Mark.O), new ComputerPlayer(Mark.X), gui);
-				//gui.changePanel(new GamePanel(newGame));
-				//new Thread(newGame).start();
-				System.out.println("choice1: " + choice1 + "choice2: " + choice2);
-				
+				if(handler!=null){
+					Game newGame = new Game(p1, p2, gui, width, height);
+					gui.changePanel(new GameMainPanel(newGame, handler));
+					new Thread(newGame).start();
+				}else{
+					Game newGame = new Game(p1, p2, gui, width, height);
+					gui.changePanel(new GameMainPanel(newGame));
+					new Thread(newGame).start();
+				}
 			}
 		});
-		
-		
-		add(newHumanPlayerLabel1);
-		add(newHumanPlayerLabel2);
-		add(newAIPlayerLabel1);
-		add(newAIPlayerLabel2);
-		add(startGameLabel);
-		//System.out.println(getPreferredSize());
-		//this.setSize(this.getPreferredSize());
-		//setSize(gui.getSize());
 
-		
-		
-		
-		
-		//this.setVisible(true);
-		
 	}
 
 	
